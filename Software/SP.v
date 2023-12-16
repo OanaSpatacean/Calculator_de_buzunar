0d765056e17231980da9f64c03fa09bf9135b7a3
CorinaOxani <corina.oxani@student.upt.ro>
2023-12-16 15:18:31 +0200

    added SP, and X,Y registers

---
+++ Software/SP.v
@@ -0,0 +1,26 @@
+module SP(
+  input CLK,RST,INC,DEC,
+  output reg [15:0] OUT);
+  
+  always @(negedge CLK, negedge RST)
+  //Dac? RST este activ, valoarea de ie?ire (OUT) este resetat? la 16'b0000000111111111.
+  //Dac? semnalul DEC este activ, valoarea de ie?ire (OUT) este decrementat? cu 1.
+  begin
+     if (!RST) begin
+        OUT <= 16'b0000000111111111;
+     end   
+   else if (DEC) begin
+        OUT <= OUT - 16'd1;
+   end 
+  end
+  
+  always @(posedge CLK) begin
+    //la fiecare front pozitiv al semnalului CLK:
+    //daca semnalul INC este activ si valoarea de ie?ire este mai mica decat 16'b0000000111111111 => valoarea de iesire ++
+    #1
+    if (INC && OUT < 16'b0000000111111111) begin
+        OUT <= OUT + 16'd1;  
+   end
+  end
+  
+endmodule
---
+++ Software/Y.v
@@ -0,0 +1,19 @@
+module Y(
+  input CLK,RST,EN,
+  input [15:0] IN,
+  output reg [15:0] OUT);
+  
+  always @(negedge CLK, negedge RST)
+  begin
+  //daca RST este activ, valoarea de ie?ire este resetata la zero
+  //daca semnalul EN este activ, valoarea de iesire preia valoarea de intrare IN
+     if (!RST) begin
+        OUT <= 16'd0;
+     end   
+   else if (EN) begin
+        OUT <= IN;  
+   end
+  end
+  
+endmodule
+
